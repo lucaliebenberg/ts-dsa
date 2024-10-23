53941a3db3617dd32f51e8702a385d960a52b3f7
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-10-23 20:12:52 +0200

    chore: quick sorting

---
+++ QuickSort.ts
@@ -0,0 +1,40 @@
+/**
+ * Quick Sort implementation
+ * 
+ * Function 1: partition
+ * Function 2: quickSort
+ */
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return;
+    }
+
+    const pivotIdx = partition(arr,lo, hi)
+    qs(arr, lo, pivotIdx - 1)
+    qs(arr, pivotIdx + 1, hi)
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+
+    let idx = lo - 1;
+
+    for (let i = 0; i < hi; i++) {
+        if (arr[i] < pivot) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
+}
