4b78aff487175464de74822c8114ea5b9ba0abcb
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-10-27 17:06:34 +0200

    chore: basic structure for array list, need to add remaining methods

---
+++ ArrayList.ts
@@ -0,0 +1,11 @@
+/**
+ * Array List implementation
+ */
+
+export default class ArrayList {
+    constructor() {}
+
+    private enqueue(item: number): void {}
+
+    private dequeue(item: number): void {}
+}
